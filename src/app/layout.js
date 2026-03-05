b9b6e2ded91f24700bf1dfeaa74cc1a8c61b93bf
darayuthhang <darayuthhang12@gmail.com>
2026-03-05 11:20:00 -0500

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,8 +26,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
- 
-<script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+ <head>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="utf-8" />
+        <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+ </head>
+
 
       <body className={`${inter.className} bg-[#F9FAFB] text-black`}>
         <AuthProvider>
